8ce53f3d3a26ed6e28ce4563fd2a118b0cfc736d
vishwasc20 <vishwasc20@gmail.com>
2020-10-29 14:04:03 +0530

    sign-up refactoring

--- src/components/pages/sign-up.js
+++ src/components/pages/sign-up.js
@@ -1,159 +1,196 @@
+// import React from 'react';
+// import { Link as RouterLink } from 'react-router-dom';
+// import Avatar from '@material-ui/core/Avatar';
+// import Button from '@material-ui/core/Button';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import TextField from '@material-ui/core/TextField';
+// import Link from '@material-ui/core/Link';
+// import Grid from '@material-ui/core/Grid';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+// import Typography from '@material-ui/core/Typography';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Container from '@material-ui/core/Container';
+// import { Divider } from '@material-ui/core';
+
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//     marginBottom: '15px'
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main
+//   },
+//   form: {
+//     width: '100%', // Fix IE 11 issue.
+//     marginTop: theme.spacing(1),
+//     marginBottom: theme.spacing(2)
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//     textTransform: 'none',
+//     fontSize: '18px'
+//   },
+//   heroButton: {
+//     backgroundColor: '#ffffff',
+//     marginTop: theme.spacing(2),
+//     textTransform: 'none'
+//   },
+//   listButton: {
+//     justifyContent: 'center'
+//   },
+//   listButton1: {
+//     justifyContent: 'center'
+//   },
+//   icon: {
+//     marginRight: '8px'
+//   }
+// }));
+
+// const SignUpView = ({ onSubmit }) => {
+//   const classes = useStyles();
+
+//   return (
+//     <Container component="main" maxWidth="xs">
+//       <CssBaseline />
+//       <div className={classes.paper}>
+//         <Avatar className={classes.avatar}>
+//           <LockOutlinedIcon />
+//         </Avatar>
+//         <Typography component="h1" variant="h5">
+//           Sign up
+//         </Typography>
+//         <form className={classes.form} noValidate onSubmit={onSubmit}>
+//           <Grid container spacing={2}>
+//             <Grid item xs={12} sm={6}>
+//               <TextField
+//                 autoComplete="fname"
+//                 name="firstName"
+//                 variant="outlined"
+//                 required
+//                 fullWidth
+//                 id="firstName"
+//                 label="First Name"
+//                 autoFocus
+//               />
+//             </Grid>
+//             <Grid item xs={12} sm={6}>
+//               <TextField
+//                 variant="outlined"
+//                 required
+//                 fullWidth
+//                 id="lastName"
+//                 label="Last Name"
+//                 name="lastName"
+//                 autoComplete="lname"
+//               />
+//             </Grid>
+//             <Grid item xs={12}>
+//               <TextField
+//                 variant="outlined"
+//                 required
+//                 fullWidth
+//                 id="email"
+//                 label="Email Address"
+//                 name="email"
+//                 type="email"
+//                 placeholder="Email"
+//                 autoComplete="email"
+//               />
+//             </Grid>
+//             <Grid item xs={12}>
+//               <TextField
+//                 variant="outlined"
+//                 required
+//                 fullWidth
+//                 name="password"
+//                 label="Password"
+//                 type="password"
+//                 placeholder="Password"
+//                 id="password"
+//                 autoComplete="current-password"
+//               />
+//             </Grid>
+//           </Grid>
+//           <Button
+//             type="submit"
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             className={classes.submit}
+//           >
+//             Sign Up
+//           </Button>
+//           <Grid container justify="flex-end">
+//             <Grid item>
+//               <Link href="/sign-in" variant="body2">
+//                 Already have an account? Sign in
+//               </Link>
+//             </Grid>
+//           </Grid>
+//         </form>
+//       </div>
+//       <Divider />
+//       <div>
+//         <form className={classes.form}>
+//           <Button
+//             variant="contained"
+//             fullWidth
+//             className={classes.heroButton}
+//             component={RouterLink}
+//           >
+//             <div className={classes.icon}>
+//               <Avatar
+//                 alt="google"
+//                 src="/assets/images/btn_google_light_pressed_ios.svg"
+//               />
+//             </div>
+//             Sign up with google
+//           </Button>
+//         </form>
+//       </div>
+//     </Container>
+//   );
+// };
+
+// export default SignUpView;
+
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
+import { CssBaseline, Container, Divider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Divider } from '@material-ui/core';
+import helpers from '../../helpers';
+import SignupForm from '../sign-up-form';
+import SignupWithGoogleButton from '../sign-up-google-button';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginBottom: '15px'
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(2)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    textTransform: 'none',
-    fontSize: '18px'
-  },
-  heroButton: {
+  additionalButtonStyles: {
     backgroundColor: '#ffffff',
     marginTop: theme.spacing(2),
     textTransform: 'none'
   },
-  listButton: {
-    justifyContent: 'center'
-  },
-  listButton1: {
-    justifyContent: 'center'
-  },
-  icon: {
-    marginRight: '8px'
+  additionalSignInOptions: {
+    margin: theme.spacing(3, 0, 2),
+    textTransform: 'none',
+    fontSize: '18px'
   }
 }));
 
-const SignUpView = ({ onSubmit }) => {
+const LoginView = ({ onSubmit, t: translate }) => {
   const classes = useStyles();
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={onSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                type="email"
-                placeholder="Email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                placeholder="Password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="/sign-in" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
+      <SignupForm onSubmit={onSubmit} />
       <Divider />
-      <div>
-        <form className={classes.form}>
-          <Button
-            variant="contained"
-            fullWidth
-            className={classes.heroButton}
-            component={RouterLink}
-          >
-            <div className={classes.icon}>
-              <Avatar
-                alt="google"
-                src="/assets/images/btn_google_light_pressed_ios.svg"
-              />
-            </div>
-            Sign up with google
-          </Button>
-        </form>
+      <div className={classes.additionalSignInOptions}>
+        <SignupWithGoogleButton classes={classes.additionalButtonStyles} />
       </div>
     </Container>
   );
 };
 
-export default SignUpView;
+export default helpers(['t'])(LoginView);
---
+++ src/components/sign-up-form/index.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import {
+  Avatar,
+  Button,
+  TextField,
+  Link,
+  Grid,
+  Typography
+} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import helpers from '../../helpers';
+
+const useStyles = makeStyles((theme) => ({
+  signupFormContainer: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginBottom: '15px'
+  },
+  lockIconAvatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  signupFormElement: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(2)
+  },
+  signupFormSubmit: {
+    margin: theme.spacing(3, 0, 2),
+    textTransform: 'none',
+    fontSize: '18px'
+  }
+}));
+
+function SignupForm(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.signupFormContainer}>
+      <Avatar className={classes.lockIconAvatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        {props.t('auth.sign-in')}
+      </Typography>
+      <form
+        className={classes.signupFormElement}
+        noValidate
+        onSubmit={onSubmit}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="fname"
+              name="firstName"
+              variant="outlined"
+              required
+              fullWidth
+              id="firstName"
+              label="First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              autoComplete="lname"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              type="email"
+              placeholder="Email"
+              autoComplete="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              placeholder="Password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.signupFormSubmit}
+        >
+          {props.t('auth.sign-up')}
+        </Button>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Link href="/sign-in" variant="body2">
+              {props.t('Already have an account? Sign in')}
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
+}
+
+export default helpers(['t'])(SignupForm);
