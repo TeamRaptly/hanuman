46ad4c852a1ca1a383d674e9c7c4bbce2aee4b71
sudhiv29 <sudhim29@gmail.com>
2020-11-17 06:31:49 +0530

    Hanuman-0002: refactor - Changing the text translationto kannada in sign-in-up and burger menu pages

--- src/components/pages/about.spec.js
+++ src/components/pages/about.spec.js
@@ -3,8 +3,16 @@ import { shallow } from 'enzyme';
 import About from './about';
 
 describe('about page component', () => {
+  const WrappedComponent = About.WrappedComponent;
+  const mockLocale = {
+    'auth.about-page': 'about page'
+  };
+  const props = {
+    t: (key) => mockLocale[key]
+  };
+
   it('renders without crashing', () => {
-    const component = shallow(<About />);
+    const component = shallow(<WrappedComponent {...props} />);
 
     expect(component).toMatchSnapshot();
   });
